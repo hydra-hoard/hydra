a6e1866e65bd034947eb9cc8cb628dcaf18110cd
karanchahal <karanbleep@gmail.com>
2018-06-22 01:51:41 +0530

    Folder structure added with the setup bash script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+# creating directory structure
+echo 'creating directory structure'
+if [ ! -d ./src ]; then
+    mkdir src
+fi
+if [ ! -d ./pkg ]; then
+    mkdir pkg
+fi
+if [ ! -d ./bin ]; then
+    mkdir bin
+fi
+
+echo 'Configuring environment variables to point to this project'
+
+# Configuring GO PATH to this project 
+export GOPATH="$PWD"
+export PATH=$PATH:$(go env GOPATH)/bin
