4b6dcb99aaef634a33c40659b71afb043bc13c40
karanchahal <karanbleep@gmail.com>
2018-07-21 18:15:11 +0530

    Append to log functionality added with unit tests

--- persistance/persistance.go
+++ persistance/persistance.go
@@ -1,15 +1,127 @@
 package persistance
 
-import "hydra-dht/structures"
+import (
+	"encoding/binary"
+	"fmt"
+	"hydra-dht/constants"
+	pb "hydra-dht/protobuf/node"
+	"hydra-dht/structures"
+	"os"
 
-func AppendToLog(index int, node structures.Node, replace bool) {
-	// write to file in following format
-	if replace {
+	"github.com/golang/protobuf/proto"
+)
 
-	}
+var (
+	logFile      *os.File
+	filePosition int64
+)
+
+/*
+
+ */
+func convertNumberToBytes(i uint64) ([]byte, int) {
+	buf := make([]byte, binary.MaxVarintLen64)
+	binary.PutUvarint(buf, i)
+
+	return buf, constants.LOG_OBJECT_BYTE_SIZE
+
 }
 
-func periodicSync() {
+/*
+AppendToLog appends a DHT entry into the persistent transaction log.
+
+The function first adds the number of bytes the LogObject is of, then inserts
+the LogObject into the log file.
+
+This method helps when we're reading the contents of the log back into memory.
+As the Log Object does not have a fixed size.
+
+Arguments:
+1. node :  Node object inside the DHT
+2. dhtIndex: Bucket Index of DHT table into which to put the node into
+3. listIndex: The index in the Bucket List of DHT in which the Node is added to.
+
+Returns:
+1. error: Returns error , if no error then error is nil
+
+*/
+func AppendToLog(node structures.Node, dhtIndex int32, listIndex int32) error {
+	// write to file in following format
+	logObject := &pb.LogNode{
+		Node: &pb.Node{
+			NodeId: node.Key[:],
+			Domain: node.Domain,
+			Port:   int32(node.Port),
+		},
+		DhtIndex:  dhtIndex,
+		ListIndex: listIndex,
+	}
+
+	out, err := proto.Marshal(logObject)
+
+	if err != nil {
+		return err
+	}
+	// the number of bytes consisting of logObject
+	i := uint64(len(out))
+	buf, _ := convertNumberToBytes(i)
+	fmt.Println(buf)
+	// writing the size of logObject into file
+	n2, err := logFile.Write(buf)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Wrote %d bytes for the size of LogObject\n", n2)
+
+	// writing the logObject into file
+	n3, err := logFile.Write(out)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Wrote LogObject into file of size %d bytes\n", n3)
+	err = logFile.Sync()
+
+	return err
+}
+
+/*
+ReadLatestObjectFromLog reads and sends the latest / last log Object inside
+the log file.
+
+Arguments:
+None
+Returns:
+1. logNode = The Log node object retrieved from the log file. It will be empty
+if there is an error
+2. error = Error object. Will be nil if no error.
+
+*/
+func ReadLatestObjectFromLog() (*pb.LogNode, error) {
+
+	logFile.Seek(filePosition, 0)
+
+	sizeOfLogObjectBuffer := make([]byte, constants.LOG_OBJECT_BYTE_SIZE)
+	logFile.Read(sizeOfLogObjectBuffer)
+	filePosition += constants.LOG_OBJECT_BYTE_SIZE
+
+	sizeOfLogObject, _ := binary.Uvarint(sizeOfLogObjectBuffer)
+	filePosition += int64(sizeOfLogObject)
+
+	logObjectBuffer := make([]byte, sizeOfLogObject)
+	logFile.Read(logObjectBuffer)
+
+	logObject := &pb.LogNode{}
+	err := proto.Unmarshal(logObjectBuffer, logObject)
+
+	return logObject, err
+}
+
+func LoadDHT() {
+	// load objects one by one and insert into DHT
+
+}
+
+func PeriodicSyncDHT() {
 	// clear log
 }
 
@@ -21,6 +133,23 @@ func flushDataStructureToDisk() {
 	//  gob ?
 }
 
-func InitPersistance() {
+func InitPersistance(filename string) error {
 	//  setup periodic flushing to disk
+	// open file
+	var err error
+	logFile, err = os.Create(filename)
+	return err
+}
+
+func ClosePersistance() {
+	logFile.Close()
+}
+
+func main() {
+
+	InitPersistance("log_1")
+
+	// do some stuff here
+
+	ClosePersistance()
 }
--- persistance/persistance_test.go
+++ persistance/persistance_test.go
@@ -1,30 +1,81 @@
 package persistance_test
 
 import (
+	"hydra-dht/persistance"
+	"hydra-dht/structures"
+	"os"
 	"testing"
 )
 
 func TestAppendToFile(t *testing.T) {
-	filename := "log.txt"
-	// init persistance
-	// append value to log
-	// read text value back up, check latest value
-	// clean up
+	filename := "log_test"
+	persistance.InitPersistance(filename)
+
+	key := structures.NodeID{5, 4, 67, 124, 234, 4, 67, 124, 234, 4, 67, 124, 234, 4, 67, 124, 234, 4, 67, 124, 234, 4, 67, 124, 234, 4, 67, 124, 234, 4, 67, 124}
+
+	var tests = []struct {
+		nodeId    byte
+		domain    string
+		port      int32
+		dhtIndex  int32
+		listIndex int32
+	}{
+
+		{1, "127.0.0.1", 10, 1, 1},
+		{2, "127.0.0.1", 20, 2, 1},
+		{3, "127.0.0.1", 30, 3, 2},
+		{4, "127.0.0.1", 40, 4, 3},
+		{5, "127.0.0.1", 50, 5, 4},
+		{6, "127.0.0.1", 60, 6, 4},
+		{7, "127.0.0.1", 70, 7, 6},
+		{8, "127.0.0.1", 80, 8, 6},
+	}
+	for _, test := range tests {
+		key[0] = test.nodeId
+
+		node := structures.Node{
+			Key:    key,
+			Domain: test.domain,
+			Port:   int(test.port),
+		}
+
+		err := persistance.AppendToLog(node, test.dhtIndex, test.listIndex)
+		if err != nil {
+			t.Errorf("%v", err)
+		}
+
+		logObject, err := persistance.ReadLatestObjectFromLog()
+		if err != nil {
+			t.Errorf("%v", err)
+		}
+
+		if logObject.Node.Domain != test.domain || logObject.DhtIndex != test.dhtIndex || logObject.ListIndex != test.listIndex || logObject.Node.Port != test.port {
+
+			t.Errorf("AppendToLog => ||  (Domain)= %v;want %v | (Port)= %v;want %v | (DHTIndex)= %v;want %v | (ListIndex)= %v;want %v",
+				logObject.Node.Domain, test.domain,
+				logObject.Node.Port, test.port,
+				logObject.DhtIndex, test.dhtIndex,
+				logObject.ListIndex, test.listIndex)
+		}
+
+	}
+	// clean up test log files
+	os.Remove(filename)
 }
 
-func TestPeriodicFlushDHT() {
+func TestPeriodicSyncDHT(t *testing.T) {
 	// set time duration
 	// check if DHT exists
 	// check after some time
 	// DHT object should be saved to disk
 }
 
-func TestClearLog() {
+func TestClearLog(t *testing.T) {
 	// clear log
 	// check if log cleared
 }
 
-func TestLoadDHT() {
+func TestLoadDHT(t *testing.T) {
 	// save to disk
 	// read it back
 	// check
