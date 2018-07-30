1197e40df8040e3f7248690a4ab14ad2ae84c598
karanchahal <karanbleep@gmail.com>
2018-07-31 02:44:25 +0530

    Corrected a small typo in build script

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
-#install libraries
-go get -d -v ./...
-go get -u github.com/golang/protobuf/protoc-gen-go
-
 # generate proto files
 cd protobuf
 sh proto.sh
+cd ..
 
+# make log and ddht persistance directory structures
 cd persistance
 mkdir log
 mkdir dht
+
+#install libraries
+go get -d -v ./...
+go get -u github.com/golang/protobuf/protoc-gen-go
 
